32b503e77cd52ad78650b871042fba164253a230
KernalsEgg <52105649+KernalsEgg@users.noreply.github.com>
2021-04-13 03:51:36 +0930

    2021-04-13 EnchantmentChargeFix

---
+++ src/PCH.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+
+#include <spdlog/sinks/basic_file_sink.h>
+
+using namespace std::literals;
+
+#define DLLEXPORT __declspec(dllexport)
